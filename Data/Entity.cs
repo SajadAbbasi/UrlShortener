cd266823b03a89902d73e3e2f6bf17ec9a455755
sajad <sajadabbasi2012@yahoo.com>
2019-06-01 19:20:48 +0430

    First Project

---
+++ Data/Entity.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+using Model;
+namespace Data
+{
+    public class Entity
+    {
+        public class EFFirstContext : DbContext
+        {
+            public EFFirstContext()
+            { }
+
+            public DbSet<Url> Urls { get; set; }
+
+        }
+    }
+}
---
+++ Model/Url.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Url
+    {
+        public long UrlId { get; set; }
+        public string LongUrl { get; set; }
+        public string ShortUrl { get; set; }
+        public string CreateDateTime { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
